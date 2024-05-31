35914e72da4682deb9fa339fd887d68e4e4d19db
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-05-31 21:00:13 +0200

    refactoring MultiplicationTable class

--- FirstJavaProjectWithEclipse/src/package1/MultiplicationTable.java
+++ FirstJavaProjectWithEclipse/src/package1/MultiplicationTable.java
@@ -3,8 +3,16 @@ package package1;
 public class MultiplicationTable {
  
 	 void print() {
-		 for (int i = 1; i<=10; i++) {
-			 System.out.printf("%d + %d = %d", 5, i, 5*i).println();
-		 }
+			print(5);
+	 }
+	 
+	 void print(int table) {
+			print(table, 1, 10);
+		}
+
+		void print(int table, int from, int to) {
+			for (int i = from; i <= to; i++) {
+				System.out.printf("%d * %d = %d", table, i, table * i).println();
+			}
 	 }
 }
--- FirstJavaProjectWithEclipse/src/package1/MultiplicationTableRunner.java
+++ FirstJavaProjectWithEclipse/src/package1/MultiplicationTableRunner.java
@@ -4,7 +4,9 @@ public class MultiplicationTableRunner {
 
 	public static void main(String[] args) {
          MultiplicationTable table = new MultiplicationTable();
-         table.print(); 
+			// table.print();
+			// table.print(7);
+			table.print(2, 2, 9);
 	}
 
 }
