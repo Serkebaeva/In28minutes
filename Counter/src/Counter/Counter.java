51c70ee25b81d236e59748883a4825e813b60f2b
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-06-02 09:25:46 +0200

    Counter assignment

--- Counter/src/Counter/Counter.java
+++ Counter/src/Counter/Counter.java
@@ -1,37 +1,55 @@
 package Counter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
-
 public class Counter {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
+		if (args.length == 0) {
+			System.out.println("No file name provided!");
+			return;
+		}
 
-		File file = new File("C:\\Users\\user\\Desktop\\sandBox\\01.txt");
+		String fileName = args[0];
+		File file = new File("C:\\aisData\\in28Minutes\\Counter\\src\\Counter\\" + fileName);
+
 		Scanner scan = new Scanner(file);
+		String string1 = scan.nextLine();
 
-		File file2 = new File("C:\\Users\\user\\Desktop\\sandBox\\02.txt");
-		Scanner scan2 = new Scanner(file2);
-
-		String String1 = scan.nextLine();
-		String String2 = scan2.nextLine();
-
-		// Calling the CharCounter method
-		Counter counter = new Counter();
-		counter.charCounter(String1);
-		System.out.println("");
-		counter.charCounter(String2);
+		if (fileName.equals("01.txt")) {
+			charCounter(string1);
+		} else {
+			charCounter2(string1);
+		}
 
 		scan.close();
-		scan2.close();
 	}
 
-	// Method to count character occurrences
-	public void charCounter(String str) {
+	// Methods to count character occurrences
+	public static void charCounter(String str) {
 		int lowerCase_a = 0;
 		int lowerCase_b = 0;
 		int lowerCase_c = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			char currentChar = str.charAt(i);
+			if (currentChar == 'a') {
+				lowerCase_a++;
+			} else if (currentChar == 'b') {
+				lowerCase_b++;
+			} else if (currentChar == 'c') {
+				lowerCase_c++;
+			}
+		}
+
+		System.out.printf("Occurrences of 'a': %d%n", lowerCase_a);
+		System.out.printf("Occurrences of 'b': %d%n", lowerCase_b);
+		System.out.printf("Occurrences of 'c': %d%n", lowerCase_c);
+
+	}
+
+	public static void charCounter2(String str) {
 		int lowerCase_h = 0;
 		int lowerCase_w = 0;
 		int lowerCase_e = 0;
@@ -52,23 +70,14 @@ public class Counter {
 				lowerCase_l++;
 			} else if (currentChar == 'o') {
 				lowerCase_o++;
-			} else if (currentChar == 'a') {
-				lowerCase_a++;
-			} else if (currentChar == 'b') {
-				lowerCase_b++;
-			} else if (currentChar == 'c') {
-				lowerCase_c++;
 			} else if (currentChar == 'r') {
 				lowerCase_r++;
 			} else if (currentChar == 'd') {
 				lowerCase_d++;
 			}
+
 		}
 
-		// Printing the results
-		System.out.printf("Occurrences of 'a': %d%n", lowerCase_a);
-		System.out.printf("Occurrences of 'b': %d%n", lowerCase_b);
-		System.out.printf("Occurrences of 'c': %d%n", lowerCase_c);
 		System.out.printf("Occurrences of 'h': %d%n", lowerCase_h);
 		System.out.printf("Occurrences of 'w': %d%n", lowerCase_w);
 		System.out.printf("Occurrences of 'e': %d%n", lowerCase_e);
@@ -76,5 +85,7 @@ public class Counter {
 		System.out.printf("Occurrences of 'o': %d%n", lowerCase_o);
 		System.out.printf("Occurrences of 'r': %d%n", lowerCase_r);
 		System.out.printf("Occurrences of 'd': %d%n", lowerCase_d);
+
 	}
+
 }
