fb89d069e1456ecc2fd5ce8702019e956a4816b6
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-06-08 17:33:42 +0200

    Counter assignment

--- Counter/src/Counter/Counter.java
+++ Counter/src/Counter/Counter.java
@@ -12,81 +12,40 @@ public class Counter {
 			return;
 		}
 
+
 		String fileName = args[0];
-		File file = new File("C:\\aisData\\in28Minutes\\Counter\\src\\Counter\\" + fileName);
-
+		File file = new File(fileName);
 		Scanner scan = new Scanner(file);
-		String string1 = scan.nextLine();
+		int[] charCount = new int[26]; // Array to store counts for a-z
 
-		if (fileName.equals("01.txt")) {
-			charCounter(string1);
-		} else {
-			charCounter2(string1);
+		while (scan.hasNextLine()) {
+			String string1 = scan.nextLine();
+			for (char ch : string1.toCharArray()) {
+				if (Character.isLowerCase(ch)) {
+					charCount[ch - 'a']++;
+				}
+			}
+		}
+
+		for (int i = 0; i < charCount.length; i++) {
+			if (charCount[i] > 0) {
+				char letter = (char) (i + 'a');
+				System.out.println(letter + ": " + charCount[i]);
+
+				/*
+				 * Conversion to Character: (i + 'a') performs arithmetic with a character and
+				 * an integer. Since 'a' is 97 (its ASCII value), adding i to 'a' yields the
+				 * ASCII value of the corresponding letter. Casting to char: The result of (i +
+				 * 'a') is an integer, but we want to treat it as a character. So, we cast it to
+				 * char. The cast (char) (i + 'a') converts the resulting integer back to a
+				 * character.
+				 */
+			}
 		}
 
 		scan.close();
 	}
-
-	// Methods to count character occurrences
-	public static void charCounter(String str) {
-		int lowerCase_a = 0;
-		int lowerCase_b = 0;
-		int lowerCase_c = 0;
-
-		for (int i = 0; i < str.length(); i++) {
-			char currentChar = str.charAt(i);
-			if (currentChar == 'a') {
-				lowerCase_a++;
-			} else if (currentChar == 'b') {
-				lowerCase_b++;
-			} else if (currentChar == 'c') {
-				lowerCase_c++;
-			}
-		}
-
-		System.out.printf("Occurrences of 'a': %d%n", lowerCase_a);
-		System.out.printf("Occurrences of 'b': %d%n", lowerCase_b);
-		System.out.printf("Occurrences of 'c': %d%n", lowerCase_c);
-
-	}
-
-	public static void charCounter2(String str) {
-		int lowerCase_h = 0;
-		int lowerCase_w = 0;
-		int lowerCase_e = 0;
-		int lowerCase_l = 0;
-		int lowerCase_o = 0;
-		int lowerCase_r = 0;
-		int lowerCase_d = 0;
-
-		for (int i = 0; i < str.length(); i++) {
-			char currentChar = str.charAt(i);
-			if (currentChar == 'h') {
-				lowerCase_h++;
-			} else if (currentChar == 'w') {
-				lowerCase_w++;
-			} else if (currentChar == 'e') {
-				lowerCase_e++;
-			} else if (currentChar == 'l') {
-				lowerCase_l++;
-			} else if (currentChar == 'o') {
-				lowerCase_o++;
-			} else if (currentChar == 'r') {
-				lowerCase_r++;
-			} else if (currentChar == 'd') {
-				lowerCase_d++;
-			}
-
-		}
-
-		System.out.printf("Occurrences of 'h': %d%n", lowerCase_h);
-		System.out.printf("Occurrences of 'w': %d%n", lowerCase_w);
-		System.out.printf("Occurrences of 'e': %d%n", lowerCase_e);
-		System.out.printf("Occurrences of 'l': %d%n", lowerCase_l);
-		System.out.printf("Occurrences of 'o': %d%n", lowerCase_o);
-		System.out.printf("Occurrences of 'r': %d%n", lowerCase_r);
-		System.out.printf("Occurrences of 'd': %d%n", lowerCase_d);
-
-	}
-
 }
+
+
+
