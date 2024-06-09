87413f2f06fe954880bc1162d6bcfc0ace6c332e
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-06-09 21:26:40 +0200

    Student class - arrays

---
+++ Student/src/student/Student.java
@@ -0,0 +1,21 @@
+package student;
+
+import java.util.Arrays;
+
+public class Student {
+	private String name;
+	int[] listOfMarks = new int[3];
+
+	public Student(String name, int[] listOfMarks) {
+		this.name = name;
+		this.listOfMarks = this.listOfMarks;
+		listOfMarks = 
+
+	}
+
+	public void getNumberOfMarks() {
+		System.out.println(Arrays.toString(listOfMarks));
+	}
+
+
+}
---
+++ Student/src/student/StudentRunner.java
@@ -0,0 +1,10 @@
+package student;
+
+import student.Student;
+
+public class StudentRunner {
+	public static void main(String[] args) {
+		Student student = new Student("Oleg", new int {2, 3, 5});
+	}
+
+}
