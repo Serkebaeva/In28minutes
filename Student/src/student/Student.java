bc69ca374d18be7af69c41d4573da43fd62a3a4a
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-06-10 09:18:55 +0200

    Student class update with array

--- Student/src/student/Student.java
+++ Student/src/student/Student.java
@@ -4,17 +4,58 @@ import java.util.Arrays;
 
 public class Student {
 	private String name;
-	int[] listOfMarks = new int[3];
+	private int[] listOfMarks;
 
 	public Student(String name, int[] listOfMarks) {
 		this.name = name;
-		this.listOfMarks = this.listOfMarks;
-		listOfMarks = 
-
+		this.listOfMarks = listOfMarks;
 	}
 
-	public void getNumberOfMarks() {
+	public int getNumberOfMarks() {
 		System.out.println(Arrays.toString(listOfMarks));
+		return listOfMarks.length;
+	}
+
+	public int getTotalSumOfMarks(){
+		int sum = 0;		
+		for(int mark: listOfMarks){
+			sum = sum + mark;
+		} 
+		return sum;
+	}
+
+	public int getMaximumMark(){
+		int max = listOfMarks[0];
+		for (int i = 1; i < listOfMarks.length; i++){
+			if (listOfMarks[i]> max){
+				max = listOfMarks[i];
+			}
+			
+		} 
+		System.out.println("Maximum mark of student: "+name+" is: "+max);
+		return max;
+	}
+
+	public int getMinimumMark(){
+		int min = listOfMarks[0];
+		for (int i = 1; i < listOfMarks.length; i++){
+			if (listOfMarks[i]< min){
+				min = listOfMarks[i];
+			}
+			
+		} 
+		System.out.println("Minimum mark of student: "+name+" is: "+min);
+		return min;
+	}
+
+	public int getAverageMark(){
+		int averageMark = 0;
+		for (int i=0; i<listOfMarks.length; i++){
+			int sum = getTotalSumOfMarks();
+			averageMark = sum/ (listOfMarks.length);
+		}
+		System.out.println("Average Mark of student: "+name+" is: "+averageMark);
+		return averageMark;
 	}
 
 
--- Student/src/student/StudentRunner.java
+++ Student/src/student/StudentRunner.java
@@ -3,8 +3,16 @@ package student;
 import student.Student;
 
 public class StudentRunner {
+
+	
 	public static void main(String[] args) {
-		Student student = new Student("Oleg", new int {2, 3, 5});
+		int[] marks = {2, 3, 5};
+		Student student = new Student("Oleg", marks);		
+		System.out.println("Number of marks: "+student.getNumberOfMarks());
+		System.out.println("Total sum: "+student.getTotalSumOfMarks());
+		student.getMaximumMark();
+		student.getMinimumMark();
+		student.getAverageMark();
 	}
 
 }
