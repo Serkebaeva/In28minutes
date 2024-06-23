5cd6c3a0ee46830e45925f6d28cd508d1b618546
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-06-23 09:40:31 +0200

    exercise triangleValidator update

--- exercise1/src/TriangleValidator/TriangleValidator.java
+++ exercise1/src/TriangleValidator/TriangleValidator.java
@@ -9,12 +9,23 @@ public class TriangleValidator {
 		return angle1 + angle2 + angle3 == 180;
 	}
 
-	public boolean isRightAngled(int side1, int side2, int side3) {
+	public boolean isRightAngledTriangle(int side1, int side2, int side3) {
+		if ((side1 * side1) + (side2 * side2) == (side3 * side3)) {
+			return true;
+		}
+		if ((side2 * side2) + (side3 * side3) == (side1 * side1)) {
+			return true;
+		}
+		if ((side1 * side1) + (side3 * side3) == (side2 * side2)) {
+			return true;
+		}
+		return false;
 
 	}
 
 	public static void main(String[] args) {
 		TriangleValidator triangle = new TriangleValidator();
 		System.out.println(triangle.isValidTriangle(60, 40, 80));
+		System.out.println(triangle.isRightAngledTriangle(3, 4, 5)); // true
 	}
 }
