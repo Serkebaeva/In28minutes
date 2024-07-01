e67f9651336b19534559a4ef411d9a39c83ce57c
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-07-01 04:55:16 +0200

    interface Introduction - exercise complexAlgorithm

---
+++ interface_Introduction/src/complexAlgorithm/DummyAlgorithm.java
@@ -0,0 +1,10 @@
+package complexAlgorithm;
+
+public class DummyAlgorithm implements ComplexAlgorithm {
+
+	@Override
+	public int complexAlgorithm(int number1, int number2) {
+		return number1 + number2;
+	}
+
+}
---
+++ interface_Introduction/src/complexAlgorithm/Project.java
@@ -0,0 +1,11 @@
+package complexAlgorithm;
+
+public class Project {
+
+	public static void main(String[] args) {
+		ComplexAlgorithm algorithm = new DummyAlgorithm();
+		System.out.println(algorithm.complexAlgorithm(10, 20));
+
+	}
+
+}
