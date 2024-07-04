ab249662e2815cdaa46f8514c7b0ba1d415d8f3c
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-07-04 19:05:11 +0200

    lengthOfLastWord

---
+++ exercise1/src/lengthOfLastWord/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+package lengthOfLastWord;
+
+public class LengthOfLastWord {
+
+	public static int lengthOfLastWord(String s) {
+		String[] arr = s.split(" ");
+		String lastWord = arr[arr.length - 1];
+		return lastWord.length();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(lengthOfLastWord("Hello World"));
+		System.out.println(lengthOfLastWord(" fly me   to   the moon"));
+		System.out.println(lengthOfLastWord("luffy is still joyboy"));
+	}
+}
