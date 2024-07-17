27335370b16127f3b84477ec3a37b3a437b013df
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-07-17 06:17:14 +0200

    exercise functional programming

---
+++ functional_programming/src/functional_programming/FunctionalProgrammingRunner.java
@@ -0,0 +1,26 @@
+package functional_programming;
+
+import java.util.List;
+
+public class FunctionalProgrammingRunner {
+
+	public static void main(String[] args) {
+		List<String> list = List.of("Apple", "Banana", "Cat", "Dog");
+		printWithFP(list);
+
+	}
+
+	private static void printBasic(List<String> list) {
+		for (String string : list) {
+			System.out.println(string);
+		}
+	}
+
+	private static void printWithFP(List<String> list) {
+		// Important syntax - Lambda expression..
+		list.stream().forEach(element -> System.out.println(" Element: " + element));
+		// We are sending the code, we are sending for each element execute this...
+		// We are sending a function code to forEach.
+	}
+
+}
