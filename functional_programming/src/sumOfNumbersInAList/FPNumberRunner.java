d46cfa43bbb9dc9f913af46b5b4716960cc3c4fd
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-07-19 05:52:17 +0200

    functional_programming exercise FPNumberRunner.java

--- functional_programming/src/sumOfNumbersInAList/FPNumberRunner.java
+++ functional_programming/src/sumOfNumbersInAList/FPNumberRunner.java
@@ -3,11 +3,24 @@ package sumOfNumbersInAList;
 import java.util.List;
 
 public class FPNumberRunner {
+	private static int normalSum(List<Integer> numbers) {
+		int sum = 0;
+		for (int number : numbers) {
+			sum += number;
+		}
+		return sum;
+	}
+
+	private static int FPSum(List<Integer> numbers) {
+		int sum = numbers.stream().reduce(0, (number1, number2) -> number1 + number2);
+		return sum;
+	}
 
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(4, 6, 8, 13, 3, 15);
-		numbers.stream().forEach(element -> System.out.println(element));
-
+		
+		System.out.println("Normal Sum: " + normalSum(numbers));
+		System.out.println("FP Sum: " + FPSum(numbers));
 	}
 
 }
